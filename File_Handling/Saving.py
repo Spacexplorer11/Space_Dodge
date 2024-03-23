7ff196621e6043d91b2dd68b83c16485f7900a05
TNTISGOOD <supersingh196@outlook.com>
2024-03-23 20:34:56 +0000

    Revert "i am gonna delete this stupid git stuff"
    This reverts commit 7d71015de89dd03e71d2df33dd7cf9ba4ed9bd29.

--- File_Handling/Saving.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-def save_object(score):
-    try:
-        with open("File_Handling/highscore.pickle", "wb") as f:
-            pickle.dump(score, f)
-    except Exception as ex:
-        print("Error during pickling object (Possibly unsupported):", ex)
