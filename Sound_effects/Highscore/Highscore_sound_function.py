d6dbfc5001a49a54ca1e7e14bfde55ab9b51c483
TNTISGOOD <supersingh196@outlook.com>
2024-03-23 21:46:51 +0000

    Fixed the simpleaudio crash issue

--- Sound_effects/Highscore/Highscore_sound_function.py
+++ Sound_effects/Highscore/Highscore_sound_function.py
@@ -5,4 +5,4 @@ def highscore_sound():
     filename = "Sound_effects/Highscore/highscore.wav"
     sound = sa.WaveObject.from_wave_file(filename)
     play_sound = sound.play()
-    play_sound.wait_done() # Wait for the sound to finish
+    play_sound.wait_done()  # Wait for the sound to finish
