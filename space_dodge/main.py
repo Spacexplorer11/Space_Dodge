6c9ac6f9adcaba701dd249e2c997c9eaac7ad6cd
Space <156950243+Spacexplorer11@users.noreply.github.com>
2025-02-18 13:17:01 +0000

    Added mini-while loops which account for the quit event while the gam… (#15)
    * Added mini-while loops which account for the quit event while the game is "paused" to display a message
    
    * Added comments
    
    * Update main.py
    
    Fixed a typo
    
    Signed-off-by: Space <156950243+Spacexplorer11@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Space <156950243+Spacexplorer11@users.noreply.github.com>

--- space_dodge/main.py
+++ space_dodge/main.py
@@ -136,7 +136,14 @@ def main():
                 pygame.display.update()
                 pygame.mixer.Sound.play(highscoreSound)
                 highscoreSoundPlayed = True
-                pygame.time.delay(1000)
+                startTime1 = time.time()
+                while not time.time() > startTime1 + 1:  # A while loop which waits for 1 second
+                    for event in pygame.event.get():   # but the game can still be quit during this time
+                        if event.type == pygame.QUIT:
+                            running = False
+                            break
+                    if not running:
+                        break
 
         if bulletCount > bulletAddIncrement:
             for _ in range(3):
@@ -168,7 +175,14 @@ def main():
                         WINDOW.blit(lostLivesText if lives > 1 else lostLifeText,
                                     (50, HEIGHT / 2 - lostLivesText.get_height()))
                         pygame.display.update()
-                        pygame.time.delay(1000)
+                        startTime1 = time.time()
+                        while not time.time() > startTime1 + 1:  # A while loop which waits for 1 second
+                            for event in pygame.event.get():   # but the game can still be quit during this time
+                                if event.type == pygame.QUIT:
+                                    running = False
+                                    break
+                            if not running:
+                                break
                     else:
                         WINDOW.blit(game_background, (0, 0))
                         pygame.display.update()
@@ -188,7 +202,14 @@ def main():
                         pygame.display.update()
                         pygame.mixer.Sound.play(GameOverSound)
                         pygame.mixer.Sound.play(sadSound)
-                        pygame.time.delay(5000)
+                        startTime1 = time.time()
+                        while not time.time() > startTime1 + 5:  # A while loop which waits for 5 seconds
+                            for event in pygame.event.get():   # but the game can still be quit during this time
+                                if event.type == pygame.QUIT:
+                                    running = False
+                                    break
+                            if not running:
+                                break
                         lives = 4
                         break
 
