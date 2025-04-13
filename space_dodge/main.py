0c6f857857b6dad698816e07e8337024f465569a
Space <spacexplorer11@proton.me>
2025-04-13 17:08:48 +0100

    Simplify Installation (#28)
    * Made so the installation is easier, you now only need to download python and can run the game, it will install packages ( if not present ) automatically. Updated the README.md, simplifying the instructions
    
    * Update main.py
    
    Added a simple comment explaining the new code
    
    Signed-off-by: Space <spacexplorer11@proton.me>
    
    * Added a new part where the script will automatically make a venv and install packages there to protect your computer. I have also warned the user in the README.md.
    
    * Changed the indentation so it installs packages after creating a venv, as previously it created a venv, restarted, but then didn't install the packages since the condition of `sys.prefix == sys.base_prefix` was false
    
    * Update README.md
    
    Removed an unnecessary line of text
    
    Signed-off-by: Space <spacexplorer11@proton.me>
    
    * Added an else clause so that if a venv exists, it will run inside it, but not create a new one
    
    ---------
    
    Signed-off-by: Space <spacexplorer11@proton.me>

--- space_dodge/main.py
+++ space_dodge/main.py
@@ -1,5 +1,35 @@
-# imports
+# Imports
+import os
+import sys
+import subprocess
+import venv
 import time
+
+# Check if running inside a virtual environment
+if sys.prefix == sys.base_prefix:
+    venv_path = os.path.join(os.path.dirname(__file__), 'venv')
+
+    # If venv doesn't exist, create it and restart the script
+    if not os.path.isdir(venv_path):
+        print("🔧 Creating a virtual environment...")
+        venv.create(venv_path, with_pip=True)
+        print("⚙️ Restarting the script inside the virtual environment...")
+        activate_script = os.path.join(venv_path, 'bin', 'python') if sys.platform != 'win32' else os.path.join(venv_path, 'Scripts', 'python.exe')
+        os.execv(activate_script, [activate_script] + sys.argv)
+    else:
+        print("⚙️ Re-starting the script inside the virtual environment...")
+        activate_script = os.path.join(venv_path, 'bin', 'python') if sys.platform != 'win32' else os.path.join(
+            venv_path, 'Scripts', 'python.exe')
+        os.execv(activate_script, [activate_script] + sys.argv)
+
+# Check if all required packages are installed in the virtual environment
+required_packages = ['pygame', 'pygame_widgets']  # List your packages here
+for package in required_packages:
+    try:
+        __import__(package)
+    except ImportError:
+        print(f"📦 Installing '{package}' inside virtual environment...")
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
 
 import pygame
 
