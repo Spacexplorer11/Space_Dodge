edef2a6131c58de11a23ff8ba3da0e9b251318e3
Spacexplorer11 <spacexplorer11@proton.me>
2025-06-23 21:47:03 +0100

    Changed the system to use JSON instead of pickle. This does mean it is easier to handle, however easier to edit as well. This supports the difficulties as JSON handles dicts easily which is needed to save separate scores for different difficulties. Simple name change for loading_func.py as well

---
+++ space_dodge/file_handling/loading.py
@@ -0,0 +1,18 @@
+import json
+
+from file_handling.utility import ref
+
+from space_dodge.file_handling.constants_and_file_loading import logger
+
+
+def load_highscore():
+    try:
+        with open(ref("file_handling/highscore.json"), "r") as f:
+            score = json.load(f)
+        print("✅ JSON highscore loaded:", score)
+        logger.info("JSON highscore loaded successfully", extra={"score": score})
+        return score, True
+    except (FileNotFoundError, json.JSONDecodeError) as ex:
+        print("❌ Failed to load JSON highscore:", ex)
+        logger.exception("Error saving JSON highscore:", exc_info=ex)
+        return 0, False
