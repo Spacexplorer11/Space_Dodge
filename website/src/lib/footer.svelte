a42b9a1a7584bc35d3876974bb29099424e69f48
Akaalroop Singh <spacexplorer11@proton.me>
2025-08-30 12:27:11 +0100

    Add a github sponsor and a buy me a coffee button to the footer!

--- website/src/lib/footer.svelte
+++ website/src/lib/footer.svelte
@@ -2,23 +2,56 @@
 	import {base} from '$app/paths';
 </script>
 <div id="footer">
+	<div id="gh-sponsor-left">
+		<!-- GitHub Sponsors -->
+		<iframe
+				src="https://github.com/sponsors/Spacexplorer11/button"
+				title="Sponsor Spacexplorer11"
+				height="32"
+				width="114"
+				style="border:0; border-radius:6px;"
+		></iframe>
+	</div>
 	<p id="footer_text">© 2025 <a href="https://github.com/spacexplorer11">Akaalroop Singh (Spacexplorer11)</a>. <br>
 		Space Dodge is licensed under the <a href="https://www.gnu.org/licenses/agpl-3.0.html">GNU AGPL v3.0</a>.
 		You can read our privacy policy <a href="{base}/privacy_policy">here</a></p>
+	<div id="bmc-right">
+		<a href="https://www.buymeacoffee.com/spacexplorer11" target="_blank" style="cursor: pointer;"><img
+				src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee"
+				style="height: 60px !important;width: 217px !important;"></a>
+	</div>
 </div>
 <style>
 	/* Footer styles */
 	#footer {
 		background: teal;
 		padding: 5px;
-		text-align: center;
-		margin-top: auto;
 		z-index: 1000;
-		position: relative;
+		width: 100%;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		box-sizing: border-box;
+		margin-top: auto;
+	}
+
+	#gh-sponsor-left {
+		display: flex;
+		align-items: center;
+		padding-left: 12px;
+	}
+
+	#bmc-right {
+		display: flex;
+		align-items: center;
+		padding-right: 12px;
 	}
 
 	#footer_text {
 		color: black;
+		flex: 1;
+		text-align: center;
+		margin: 0;
 	}
 
 	#footer a {
