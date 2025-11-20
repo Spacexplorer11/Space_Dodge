7eacc3e37bffcfaa2d786a289bcddee9e75558fd
Akaalroop Singh <akaal@akaalroop.com>
2025-11-20 18:47:03 +0000

    Correctly update header classes so css isn't affected in about page

--- website/src/routes/about/+page.svelte
+++ website/src/routes/about/+page.svelte
@@ -4,7 +4,7 @@
 	However, every single feature after that was hand-coded by me and my <a href="/contributors">contributors.</a>
 </p>
 
-<div class="header"><h2>Why did I make it?</h2></div>
+<header><h2>Why did I make it?</h2></header>
 <p>Space Dodge was made a project to assist my skills in python! I wanted to start doing more text-based
 	programming rather than scratch's block based. I wanted to start with a game, as a fun way to boost my motivation to
 	code as well!
@@ -13,21 +13,21 @@
 	I also vowed to never use ai in this project because ai doesn't really help you learn the code and feels wrong to
 	use as well. </p>
 
-<div class="header"><h2>How long did it take?</h2></div>
+<header><h2>How long did it take?</h2></header>
 <p>As mentioned before, Space Dodge was just a side project, something to boost my learning, but soon became my true
 	passion and main project.
 	I can't provide an exact time, however an estimate would be around 100ish hours. I know I've spent 30+ hours on the
 	website alone, and the game has been my focus for longer
 	so the total time must easily exceed 100 hours.</p>
 
-<div class="header"><h2>What is the future of Space Dodge?</h2></div>
+<header><h2>What is the future of Space Dodge?</h2></header>
 <p>Space Dodge is a project I want to continue, however I am getting involved with more and more projects, such as my
 	recent <a href="https://github.com/Spacexplorer11/Meow_Meals">Meow Meals</a>,
 	I haven't been able to spend as much time as I would like on Space Dodge, but I have plenty of more ideas planned
 	for it! Whenever I feel like coding,
 	and I haven't gotten an active project with a deadline, I work on Space Dodge, my first ever passion project ❤️</p>
 <style>
-	.header {
+	header {
 		display: flex;
 		justify-content: center;
 		align-content: center;
